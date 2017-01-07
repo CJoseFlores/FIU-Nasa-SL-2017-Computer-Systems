8f172961788415d902bda464c330c59e378bfe1d
Carlos Flores <cflor083@fiu.edu>
2017-01-07 16:38:40 -0500

    Created a program that easily converts RGB values to HSV needed for OpenCV

---
+++ BGR_2_HSV.py
@@ -0,0 +1,14 @@
+import argparse
+import cv2
+import numpy
+
+ap = argparse.ArgumentParser()
+ap.add_argument("Red", help="Specify the Red value of the RGB color.")
+ap.add_argument("Green",help="Specify the Green value of the RGB color.")
+ap.add_argument("Blue",help="Specify the Blue value of the RGB color.")
+
+args = vars(ap.parse_args())
+
+color = np.uint8([[[args["Blue"],args["Green"],args["Red"]]]])
+hsv_color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
+print hsv_color
